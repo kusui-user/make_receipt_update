da167bb68d1b190c73e5f6d5ed26d4ba1a486993
kusui.takashi <kusui@foodnetwork.co.jp>
2022-08-29 15:15:41 +0900

    更新

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import win32gui
 import win32.lib.win32con as win32con
-memo_app = win32gui.FindWindow(None,'d5d7097ea16b705da1d4d1b0ebe63572.pdf - 職場 - Microsoft​ Edge')    #「タイトルなし - メモ帳」という名前のウィンドウハンドルを取得
+memo_app = win32gui.FindWindow(None,'Adobe Acrobat Pro DC (32-bit)')    #「タイトルなし - メモ帳」という名前のウィンドウハンドルを取得
 win32gui.SetForegroundWindow(memo_app)                         #ウィンドウを最前面に移動してアクティブ化
 win32gui.ShowWindow(memo_app, win32con.SW_MAXIMIZE) 
 
